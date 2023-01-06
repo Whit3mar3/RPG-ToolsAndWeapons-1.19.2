e880c5a572a9e830a715f1a2d45b5dc5488bca2e
Whit3mar3 <Whit3mar3.pro@gmail.com>
2023-01-06 13:42:15 +0100

    added first version of the project

---
+++ src/main/java/com/Whit3mar3/rpgtaw/RpgtawMod.java
@@ -0,0 +1,15 @@
+package com.Whit3mar3.rpgtaw;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RpgtawMod implements ModInitializer {
+	public static final String MOD_ID = "rpgtaw";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/Whit3mar3/rpgtaw/RpgtawModClient.java
@@ -0,0 +1,10 @@
+package com.Whit3mar3.rpgtaw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RpgtawModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
